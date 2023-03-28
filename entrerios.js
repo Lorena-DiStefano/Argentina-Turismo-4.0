a343ab71c476ebc17974746e14faebec4e2f90e4
sebapitta <118943830+sebapitta@users.noreply.github.com>
2023-03-28 19:22:37 -0300

    Update entrerios.js

--- entrerios.js
+++ entrerios.js
@@ -20,31 +20,3 @@ app.get('/register.html',(req,res)=>{
 app.get('/login.html',(req,res)=>{
     res.sendFile(path.join(__dirname,'./views/entrerioslogin.html'))
 })
-
-/*
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-const port = process.env.PORT || 3003;
-
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));*/
-/*
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/home.html");
-});
-
-app.get("/",(req,res)=>{
-  res.sendFile(__dirname + "/views/register.html");
-});
-
-app.get("/",(req,res)=>{
-  res.sendFile(__dirname + "/views/login.html");
-});
-
-app.get('/register.',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/views/register.html'))
-})
-
-app.get('/login.',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/views/login.html'))
-})*/

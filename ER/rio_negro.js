e0f5d9c0f77efac36291bb3dfdbc033e04d833f7
sebapitta <118943830+sebapitta@users.noreply.github.com>
2023-04-03 18:38:32 -0300

    Actualizo Formulario, login y pagina ppal de ER

---
+++ ER/rio_negro.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showPics();
+
+function showPics() {
+
+  //let i = 0;
+  let slides = document.getElementsByClassName("images");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[slideIndex - 1].style.display = "block";
+  }
+  setTimeout(showPics, 2000);
+
+}

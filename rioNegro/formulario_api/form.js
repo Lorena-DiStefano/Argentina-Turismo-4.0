df9d82b6403edf91606903494b2752fab6a3e4d6
Lorena Di Stefano <lore.distefano@gmail.com>
2023-04-06 11:52:59 -0300

    formulario reserva-nueva versión

--- rioNegro/formulario_api/form.js
+++ rioNegro/formulario_api/form.js
@@ -1,12 +1,11 @@
 
 let idCity;
-let search = true
+//let search = true
 
 const printHotel = async () => {
 
   const card_cont = document.getElementById('api')
   const radio = document.getElementById('radioHotel')
-  
   idCity = document.getElementById('city').value
 
   try {
@@ -17,7 +16,7 @@ const printHotel = async () => {
     cityApi = apiAlojamiento.filter((e) => e.city.includes(idCity))
     console.log(cityApi)
 
-    cityApi.forEach(e=> {
+    cityApi.forEach(e => {
       let host = document.createElement('div')
       host.clasName = "card_host_cont"
       host.innerHTML = `    
@@ -31,20 +30,23 @@ const printHotel = async () => {
                   </div>`
       card_cont.append(host)
 
+      let divRadio = document.createElement('div')
+      divRadio.className = 'divRadio'
+      let labelHotel = document.createElement('label')
+      labelHotel.className = 'radioHotel'
+      labelHotel.for = 'hotel'
+      labelHotel.textContent = `${e.name}`
       let inputHotel = document.createElement('input')
       inputHotel.type = 'radio'
       inputHotel.name = "hotel"
       inputHotel.className = 'radioHotel'
       inputHotel.value = `${e.name}`
-      inputHotel.checked= true
-      let labelHotel = document.createElement('label')
-      inputHotel.className = 'radioHotel'
-      labelHotel.for = 'hotel'
-      labelHotel.textContent = `${e.name}`
-      radio.append(inputHotel)
-      radio.append(labelHotel)
-    })
+      inputHotel.checked = true
+      radio.append(divRadio)
+      divRadio.append(labelHotel)
+      divRadio.append(inputHotel)
 
+    })
     const card_title = document.getElementById('card_title')
     card_title.textContent = `Hoteles en ${idCity}`
     const radio_title = document.getElementById('radio_title')
@@ -53,23 +55,27 @@ const printHotel = async () => {
     radioH3.textContent = "Elije el Hotel"
     radio_title.append(radioH3)
 
-    const form_info = document.getElementById('form_info')
-    form_info.style.display = 'flex'
-
-    window.location.href = "#listHostel";
-    search = false
-    btnSearch.textContent ="borrar"
-    
+    //search = false
+   
   } catch (error) {
     console.log(error)
   }
 }
 
-const btnSearch = document.getElementById('btn_city')
 
-btnSearch.addEventListener('click', function () {
-  search ? printHotel() : window.location.reload()
-})
+// const form_two = document.getElementById('form_two')
+// const sec_two = document.getElementById('sec_two')
+const btnSearch = document.getElementById('btn_city')
+btnSearch.onclick = (e) => { e.preventDefault(),printHotel()}
+
+
+// btnSearch.addEventListener('click', function (e) {
+//   e.preventDefault();
+
+//   search ? printHotel() : sec_two.style.display = "none"; search = true;
+// })
+
+//=================================================================//
 
 const openModal = () => {
   modal.showModal()
@@ -79,10 +85,8 @@ const getInfo = (event) => {
   event.preventDefault()
   let hotel = document.querySelector('input[name="hotel"]:checked').value
   let roomKind = document.querySelector('input[name="room"]:checked').value
-  let firstName =( document.getElementById('name').value).toUpperCase()
+  let firstName = (document.getElementById('firstName').value).toUpperCase()
   let lastName = (document.getElementById('lastName').value).toUpperCase()
-  let address = (document.getElementById('address').value).toUpperCase()
-  let location = (document.getElementById('location').value).toUpperCase()
   let phone = document.getElementById('phone').value
   let email = document.getElementById('email').value
   let checkIn = document.getElementById("checkIn").value
@@ -95,10 +99,10 @@ const getInfo = (event) => {
   const modalData = document.createElement('div')
   modalData.innerHTML = `
 <div>
-<p><span>${firstName} ${lastName}</span>,<br>Con domicilio en <span>${address}</span>, <span>${location}</span>.<br>Teléfono: <span>${phone}</span><br>e-mail: <span>${email}</span></p>
+<p><span>${firstName} ${lastName}</span>,<br>Teléfono: <span>${phone}</span><br>e-mail: <span>${email}</span></p>
 </div>
 <div>
-En vistas de su visita a la ciudad de <span>${idCity}</span> en la Provincia de <i>Río Negro</i>, solicita la reserva de <span>${roomQ}</span> habitación(s) denominada(s) <span>${roomKind}</span> para <span>${guestQ}</span> persona(s), en <span>${hotel}</span>.</p>
+En vistas de su visita a la ciudad de <span>${idCity}</span> en la Provincia de <i>Río Negro</i>, solicita la reserva de <span>${roomQ}</span> habitación(s) denominada(s) <span>${roomKind}</span> para <span>${guestQ}</span> persona(s), en <span>"${hotel}"</span>.</p>
 <p>Con fecha de ingreso <span>${checkIn}</span> y de salida <span>${checkOut}</span>.</p>
 <p>En su mensaje refiere: <span>${message}</span>.</p>
 </div>
@@ -108,16 +112,16 @@ En vistas de su visita a la ciudad de <span>${idCity}</span> en la Provincia de 
 <p><span>La Scaloneta</span></p>
 </div>
 <button type="submit" class="btn" id="btn_close">cerrar</button>`
- data.append(modalData)
+  data.append(modalData)
   openModal()
-  const btn_close = document.getElementById('btn_close')  
+  const btn_close = document.getElementById('btn_close')
   btn_close.onclick = () => { window.location.reload() }
 }
 
 const btn_print = document.getElementById('btn_print')
 btn_print.onclick = () => { window.print() }
 
-const btn_form = document.getElementById('btn_form')
-btn_form.addEventListener('click', getInfo)
+const btn_submit = document.getElementById('btn_submit')
+btn_submit.addEventListener('click', getInfo)
 
 //=====================================================================//

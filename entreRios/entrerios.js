3c234def50659f651e5b331a9a77af28c5ff42ad
sebapitta <118943830+sebapitta@users.noreply.github.com>
2023-04-10 15:20:01 -0300

    Actualizo eR

--- entreRios/entrerios.js
+++ entreRios/entrerios.js
@@ -9,6 +9,8 @@ app.use(express.static(path.resolve(__dirname,'./public')));
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 
+
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'./views/entrerios.html'))
 })
@@ -20,3 +22,19 @@ app.get('/register.html',(req,res)=>{
 app.get('/login.html',(req,res)=>{
     res.sendFile(path.join(__dirname,'./views/entrerioslogin.html'))
 })
+$(document).ready(function() {
+
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item").click(function() {
+        $("#myCarousel").carousel(1);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function() {
+        $("#myCarousel").carousel("prev");
+    });
+
+});

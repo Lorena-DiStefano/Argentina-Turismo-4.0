93519147c6c34408cfafbb1257df156030645dd9
SilGuaGaitan <dragonrosadito@gmail.com>
2023-04-10 11:48:33 -0300

    septimo commit

--- Santa_fe/formulario.js
+++ Santa_fe/formulario.js
@@ -2,8 +2,13 @@ const button = document.querySelector("#button");
 const step1 = document.querySelector("#step-1");
 const step2 = document.querySelector("#step-2");
 const summary = document.querySelector("#summary");
+const btn_print= document.getElementById("btn_print");
+  
+//btn_print.onclick=()=>{window.print};//
+btn_print.onclick=()=>alert("Se manda a imprimir");
+const button_c= document.getElementById("button_c");
 
-
+button_c.onclick=()=>{window.location.reload};
 //agregar evento onclick al botón siguiente
 button.addEventListener("click", function() {
   // recopilar datos del primer paso
